142aeac9b3d1535100882dd5d899a11c18e70c0f
Enes <79324431+Enesbicer@users.noreply.github.com>
2025-10-21 16:08:11 +0300

    Add imports for Image, Capsule, ModelLoader, and Executor
    Added import statements for additional modules.

--- src/executors/Detection.py
+++ src/executors/Detection.py
@@ -1,6 +1,8 @@
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
+
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.capsule import Capsule
 from capsules.Tensorflow.src.utils.loads import ModelLoader
