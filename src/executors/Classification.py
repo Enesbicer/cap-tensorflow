bfa5def5651daf98ed7f9991e8e5ffad1f7bf3e8
Enes <79324431+Enesbicer@users.noreply.github.com>
2025-10-21 16:06:58 +0300

    Implement Classification capsule for image processing

---
+++ src/executors/Classification.py
@@ -0,0 +1,41 @@
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
+
+from sdks.novavision.src.media.image import Image
+from sdks.novavision.src.base.capsule import Capsule
+from capsules.Tensorflow.src.utils.loads import ModelLoader
+from sdks.novavision.src.helper.executor import Executor
+from capsules.Tensorflow.src.models.PackageModel import PackageModel
+from capsules.Tensorflow.src.utils.response import build_response_classifier
+from capsules.Tensorflow.src.classes.TensorflowVision import TensorFlowClassifier
+
+
+class Classification(Capsule):
+    def __init__(self, request, bootstrap):
+        super().__init__(request, bootstrap)
+        self.request.model = PackageModel(**(self.request.data))
+        self.model = self.bootstrap.get("model")
+        self.device = self.bootstrap.get("device")
+        self.images = self.request.get_param("inputImage")
+        self.num_predictions = self.request.get_param("NumPredictions")
+        self.model_name = self.request.get_param("ClassificationWeights")
+        self.config_model_type = self.request.get_param("ConfigClassificationModelType")
+        self.predictions = []
+
+    @staticmethod
+    def bootstrap(config: dict) -> dict:
+        model = ModelLoader(config=config).load_models()
+        return model
+
+    def run(self):
+        img = Image.get_frame(img=self.images, redis_db=self.redis_db)
+        classifier = TorchVisionClassifier(self)
+        classifier.predict(img.value, img.uID)
+        return build_response_classifier(context=self)
+
+
+if "__main__" == __name__:
+    Executor(sys.argv[1]).run()
