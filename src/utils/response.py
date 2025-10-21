9bd0dedd1eb4ae08f8e57626ab214844fc8b46ee
Enes <79324431+Enesbicer@users.noreply.github.com>
2025-10-21 14:33:07 +0300

    Refactor response building functions in response.py
    Refactor response building functions for detection, classification, and segmentation. Introduced separate functions for building responses based on the type of model.

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,43 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from capsules.Pytorch.src.models.PackageModel import (
+    PackageModel, \
+    PackageConfigs, \
+    ConfigExecutor, \
+    DetectionExecutor, ClassificationExecutor, SegmentationExecutor, \
+    DetectionResponse, ClassificationResponse, SegmentationResponse, \
+    DetectionOutputs, ClassificationOutputs, SegmentationOutputs, \
+    OutputDetections
+)
 
+def build_response_detector(context):
+    outputInfer = OutputDetections(value=context.predictions)
+    objectDetectionOutputs = DetectionOutputs(outputDetections=outputInfer)
+    objectDetectionResponse = DetectionResponse(outputs=objectDetectionOutputs)
+    objectDetectionExecutor = DetectionExecutor(value=objectDetectionResponse)
+    executor = ConfigExecutor(value=objectDetectionExecutor)
+    packageConfigs = PackageConfigs(executor=executor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
 
-def build_response(context):
-    outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
+def build_response_classifier(context):
+    outputClassificationResults = OutputDetections(value=context.predictions)
+    imageClassificationOutputs = ClassificationOutputs(outputDetections=outputClassificationResults)
+    imageClassificationResponse = ClassificationResponse(outputs=imageClassificationOutputs)
+    imageClassificationExecutor = ClassificationExecutor(value=imageClassificationResponse)
+    executor = ConfigExecutor(value=imageClassificationExecutor)
+    packageConfigs = PackageConfigs(executor=executor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+
+def build_response_segment(context):
+    outputDetections = OutputDetections(value=context.predictions)
+    objectDetectionOutputs = SegmentationOutputs(outputDetections=outputDetections)
+    objectDetectionResponse = SegmentationResponse(outputs=objectDetectionOutputs)
+    objectDetectionExecutor = SegmentationExecutor(value=objectDetectionResponse)
+    executor = ConfigExecutor(value=objectDetectionExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
